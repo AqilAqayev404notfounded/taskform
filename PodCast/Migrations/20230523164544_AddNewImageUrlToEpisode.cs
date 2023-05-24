246ccd224c95f0bfde091f984f148a50b136026c
Rafet <rafetr@risk.az>
2023-05-24 11:42:41 +0400

    Add project files.

---
+++ PodCast/Migrations/20230523164544_AddNewImageUrlToEpisode.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PodCast.Migrations
+{
+    public partial class AddNewImageUrlToEpisode : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Episodes",
+                newName: "CornerImageUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "BackgroundImageUrl",
+                table: "Episodes",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BackgroundImageUrl",
+                table: "Episodes");
+
+            migrationBuilder.RenameColumn(
+                name: "CornerImageUrl",
+                table: "Episodes",
+                newName: "ImageUrl");
+        }
+    }
+}
